e6285fa93151fe0748c9bcf30fe00d3e8e82b2d9
Si1entERA <NeoLegote@outlook.com>
2024-06-11 19:30:04 +0200

    update

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const form = document.getElementById("form");
 const email = document.getElementById("email");
 const error = document.querySelector(".error");
+
+// button
 const submit = document.getElementById("submit");
 const returnBtn = document.querySelector(".dismiss-btn");
 
-
+// success text
 const userEmail = document.querySelector(".userEmail");
 
 //displays
@@ -17,19 +19,27 @@ form.addEventListener("submit" , (e)=> {
 
   if(!email.value.match(emailReg)){
     error.style.display = 'block';
+    email.style.color = "var(--Tomato)";
+    email.style.border = "1px solid var(--Tomato)";
+
+    setTimeout(() => {
+      email.style.color = "black";
+      email.style.border = "1px solid var(--Grey)";
+      error.style.display = 'none';
+    }, 2000);
   }
   else{
     newsletter.style.display = "none";
     success.style.display = "flex";
     userEmail.innerHTML = email.value;
   }
-  console.log(email.value);
+  // console.log(email.value);
 });
 
 
 //return button
 returnBtn.addEventListener("click", () => {
-  newsletter.style.display = "block";
+  newsletter.style.display = "flex";
   email.innerHTML = "";
   success.style.display = "none";
 })
